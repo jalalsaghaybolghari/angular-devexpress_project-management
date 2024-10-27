1508f0aa69e6702caac650392bbdfb8d5b1e9670
jalal-sagaie <jalal.sagaie@gmail.com>
2024-10-27 15:54:41 +0100

    feat: implement member list component(#22)

--- src/app/features/project-managment/components/member-list/member-list.component.ts
+++ src/app/features/project-managment/components/member-list/member-list.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, inject } from '@angular/core';
+import { ProjectManagmentApiService } from '../../services/api.service';
+import { ProjectMembersResult } from '../../project-managment.model';
+import { take } from 'rxjs';
+import ArrayStore from 'devextreme/data/array_store';
+export interface DialogData {
+  projectId: number;
+}
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.scss'
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
+  @Input() projectId: number | undefined;
+  public membersData!: ProjectMembersResult[];
+  dataSource: any;
+  priority: any[] | undefined;
 
+
+
+  constructor(private projectManagmentApiService: ProjectManagmentApiService) {}
+  ngOnInit(): void {
+    this.loadInitData();
+  }
+  loadInitData() {
+    this.projectManagmentApiService
+      .getProjectMembers(this.projectId as number)
+      .pipe(take(1))
+      .subscribe({
+        next: (res) => {
+          this.membersData = res;
+          const employeesStore = new ArrayStore({
+            data: this.membersData,
+            key: 'id'
+          });
+          this.dataSource = {
+            store: employeesStore,
+            expand: 'ResponsibleEmployee',
+            select: ['id', 'userName', 'projectRole', 'fromDate', 'toDate']
+          };
+        },
+        error: (err) => console.error('Observable emitted an error: ' + err),
+      });
+    
+
+
+      this.priority = [
+        { name: 'High', value: 4 },
+        { name: 'Urgent', value: 3 },
+        { name: 'Normal', value: 2 },
+        { name: 'Low', value: 1 }
+      ];
+  }
 }
--- src/app/shared/shared.model.ts
+++ src/app/shared/shared.model.ts
@@ -1,10 +1,24 @@
 export class ToastMessage {
-    message: string;
-    messageType: 'success' | 'error' | 'info';
-    timeOut: number | null;
-    constructor() {
-        this.message = "";
-        this.messageType = 'error'
-        this.timeOut = null;       
-    }
+  message: string;
+  messageType: 'success' | 'error' | 'info';
+  timeOut: number | null;
+  constructor() {
+    this.message = '';
+    this.messageType = 'error';
+    this.timeOut = null;
+  }
+}
+
+export class ModalInput {
+  isVisable: boolean;
+  title: string;
+  component: 'addMember' | 'memberlist' | null;
+  maxWidth: number;
+
+  constructor() {
+    this.isVisable = false;
+    this.title = '';
+    this.component = null;
+    this.maxWidth = 400;
+  }
 }
